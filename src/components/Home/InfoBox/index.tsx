c4a9928408d2a87b2b12e171be9684113ea7a143
Andressa Bendette <andressa@lumosweb.com.br>
2022-09-21 19:43:24 -0300

    refactor: fixing some features

--- src/components/Home/InfoBox/index.tsx
+++ src/components/Home/InfoBox/index.tsx
@@ -12,21 +12,21 @@ export function InfoBox() {
           <li>
             <div className={styles.icon}><img src={burgerIcon} alt="Ícone de hamburger" /></div>
             <div className={styles.text}>
-              <h3>Artesanal</h3>
+              <h2>Artesanal</h2>
               <p>Nossas receitas são feitas com todo cuidado</p>
             </div>
           </li>
           <li>
             <div className={styles.icon}><img src={headsetIcon} alt="Ícone de fone de ouvido" /></div>
             <div className={styles.text}>
-              <h3>Atendimento</h3>
+              <h2>Atendimento</h2>
               <p>Totalmente personalizado</p>
             </div>
           </li>
           <li>
             <div className={styles.icon}><img src={deliveryIcon} alt="Ícone de moto de entrega" /></div>
             <div className={styles.text}>
-              <h3>DELIVERY Speed</h3>
+              <h2>DELIVERY Speed</h2>
               <p>Entregamos menos de 45 minutos</p>
             </div>
           </li>
--- src/components/Home/Location/index.tsx
+++ src/components/Home/Location/index.tsx
@@ -7,7 +7,7 @@ export function Location() {
         <h2>Onde ficar a nosso castelo</h2>
         <p>Estaremos de portas abertas para a nossa realeza.</p>
       </div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58503.32387970499!2d-46.66906365777401!3d-23.587911204762754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5f8c61020205%3A0x9ae474c606fef4!2sMercado%20Municipal%20de%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1662576852728!5m2!1spt-BR!2sbr" width="600" height="450" style={{ border: 0 }}  loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58503.32387970499!2d-46.66906365777401!3d-23.587911204762754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5f8c61020205%3A0x9ae474c606fef4!2sMercado%20Municipal%20de%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1662576852728!5m2!1spt-BR!2sbr" width="600" height="450" style={{ border: 0 }}  loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Google maps"></iframe>
     </section>
   )
 }
