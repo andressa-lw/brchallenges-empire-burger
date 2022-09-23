1b674491810a6812e89dd4355a4fa3d874231f72
Andressa Bendette <andressa@lumosweb.com.br>
2022-09-23 10:59:16 -0300

    fix: bugs deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
+import "animate.css/animate.min.css";
+
 export function App() {
   return (
     <>
