36f5594fdaab93c9ab6d2c2089e7bbd46c3a8528
Andressa Bendette <andressa@lumosweb.com.br>
2022-09-23 10:57:05 -0300

    fix: bugs deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './global.scss';
-import "animate.css/animate.min.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
