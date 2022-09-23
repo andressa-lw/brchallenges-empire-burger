72b8adce436074ed0aa983a281cee857af0295f6
Andressa Bendette <andressa@lumosweb.com.br>
2022-09-23 11:00:49 -0300

    fix: bugs deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
-import "animate.css/animate.min.css";
-
 export function App() {
   return (
     <>
--- src/components/Home/MenuFood/index.tsx
+++ src/components/Home/MenuFood/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-
+import "animate.css/animate.min.css";
 import styles from './styles.module.scss';
 
 interface ItemMenu {
